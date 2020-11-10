fede93d294b07b17d5dedbcac403f8bb877fff13
Caitlin <clkeating16@gmail.com>
2020-11-09 18:21:15 -0800

    caitlin first commit

---
+++ Assets/CameraLookAtCar.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraLookAtCar : MonoBehaviour
+{
+
+    public GameObject car;
+    Vector3 lookAtOffset; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lookAtOffset = new Vector3(0.1f, 3f, 0f); 
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.LookAt(car.transform.position + lookAtOffset);  
+    }
+}
