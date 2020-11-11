110dbc30182f7a5b8c2d4f38a6ac14717ae83a29
Changwoo Hahn <gksckddn_@naver.com>
2020-11-11 12:40:30 -0500

    Chang new scene

---
+++ Assets/Scripts/CamSplit.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamSplit : MonoBehaviour
+{
+    public Camera cam;
+    public int camNum;
+    void Start()
+    {
+        if (camNum == 1)
+        {
+            cam.rect = new Rect(0, 0f, .5f, 1f);
+        }
+        else if (camNum == 2) {
+            cam.rect = new Rect(0.5f, 0f, .5f, 1f);
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/PlayerHood.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHood : MonoBehaviour
+{
+    public Transform[] limit;
+    private Transform tr;
+    void Start()
+    {
+        tr = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (/*Input.GetAxisRaw("Horizontal") < 0*/Input.GetKey(KeyCode.J))
+        {
+            tr.position = Vector3.MoveTowards(tr.position, new Vector3(limit[0].position.x, tr.position.y, limit[0].position.z), 3*Time.deltaTime);
+        }
+        else if (/*Input.GetAxisRaw("Horizontal") > 0*/ Input.GetKey(KeyCode.L)) {
+            tr.position = Vector3.MoveTowards(tr.position, new Vector3(limit[1].position.x, tr.position.y, limit[1].position.z), 3*Time.deltaTime);
+        }
+    }
+}
