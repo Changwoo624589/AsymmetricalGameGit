c44a6456a197d19d4e2396cadac76d93505be43d
Changwoo Hahn <gksckddn_@naver.com>
2020-11-15 06:18:50 -0500

    Chang_stage1

--- Assets/Scripts/PlayerHood.cs
+++ Assets/Scripts/PlayerHood.cs
@@ -5,34 +5,36 @@ using UnityEngine;
 public class PlayerHood : MonoBehaviour
 {
     public Transform[] limit;
-    public Transform driver;
+  
     private Transform tr;
-    public ParticleSystem vomit;
-    
-    
+    private float moveSpeed =2.5f;
+    // public Transform driver;
+    // public ParticleSystem vomit;
+
+
     void Start()
     {
         
         tr = GetComponent<Transform>();
-        vomit.emissionRate = 0; 
+       // vomit.emissionRate = 0; 
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        transform.LookAt(driver);
+      /* transform.LookAt(driver);
 
         if (Input.GetKey(KeyCode.I))
         {
             vomit.Emit(15);
         }
-        
+        */
         if (/*Input.GetAxisRaw("Horizontal") < 0*/Input.GetKey(KeyCode.J))
         {
-            tr.position = Vector3.MoveTowards(tr.position, new Vector3(limit[0].position.x, tr.position.y, limit[0].position.z), 3*Time.deltaTime);
+            tr.position = Vector3.MoveTowards(tr.position, new Vector3(limit[0].position.x, tr.position.y, limit[0].position.z), moveSpeed*Time.deltaTime);
         }
         else if (/*Input.GetAxisRaw("Horizontal") > 0*/ Input.GetKey(KeyCode.L)) {
-            tr.position = Vector3.MoveTowards(tr.position, new Vector3(limit[1].position.x, tr.position.y, limit[1].position.z), 3*Time.deltaTime);
+            tr.position = Vector3.MoveTowards(tr.position, new Vector3(limit[1].position.x, tr.position.y, limit[1].position.z), moveSpeed*Time.deltaTime);
         }
     }
 }
